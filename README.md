6adb712ce1f5c39a61c3162249e9d52be3e146cd
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2021-12-22 19:01:33 -1000

    startup scripting of HTML, CSS & JS

---
+++ README.md
@@ -0,0 +1,88 @@
+# Read Me Template
+
+![Project Image](project-image-url)
+
+> This is a ReadMe template to help save you time and effort.
+
+---
+
+### Table of Contents
+You're sections headers will be used to reference location of destination.
+
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+- [License](#license)
+- [Author Info](#author-info)
+
+---
+
+## Description
+This web application has been developed with the intention only to quiz you on your wits. Contains multiple-choice options and possibly other fancy elements. 
+Quiz will be timed :) 
+
+
+#### Technologies
+
+- Technology 1
+- Technology 2
+
+[Back To The Top](#read-me-template)
+
+---
+
+## How To Use
+
+#### Installation
+
+
+
+#### API Reference
+
+20211222155641
+ https://opentdb.com/api.php?amount=10&category=29&difficulty=medium
+    <p>20211222155641</p>
+```
+[Back To The Top](#read-me-template)
+
+---
+
+## References
+[Back To The Top](#read-me-template)
+
+---
+
+## License
+
+MIT License
+
+Copyright (c) [2021/2022] [CHELSIE L BANGLOY-GONSALVES]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[Back To The Top](#read-me-template)
+
+---
+
+## Author Info
+
+- Linkedin - [chelsiebgonsalves](https://www.linkedin.com/in/chelsiebgonsalves/)
+- Website - [Blackcat-Tinkerngineers](https://github.com/Blackcat-Tinkerngineers)
+
+[Back To The Top](#read-me-template)
---
+++ assets/js/questions.js
@@ -0,0 +1,127 @@
+// 20211222155641
+// API source https://opentdb.com/api.php?amount=10&category=29&difficulty=medium
+
+//below is questions, correct and incorrect answers. 
+{
+    var questions = [
+        
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In Marvel Comics, Taurus is the founder and leader of which criminal organization?",
+            "correct_answer": "Zodiac",
+            "incorrect_answers": [
+              "Scorpio",
+              "Tiger Mafia",
+              "The Union"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What&#039;s the name of Batman&#039;s parents?",
+            "correct_answer": "Thomas &amp; Martha",
+            "incorrect_answers": [
+              "Joey &amp; Jackie",
+              "Jason &amp; Sarah",
+              "Todd &amp; Mira"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "In the webcomic Homestuck, the first character introduced is Dave Strider.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+              "True"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which issue of the &quot;Sonic the Hedgehog&quot; comic did Scourge the Hedgehog make his first appearance?",
+            "correct_answer": "Sonic the Hedgehog #11",
+            "incorrect_answers": [
+              "Sonic Universe #32",
+              "Sonic the Hedgehog #161",
+              "Sonic the Hedgehog #47"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In Calvin and Hobbes, what is the name of the babysitter&#039;s boyfriend?",
+            "correct_answer": "Charlie",
+            "incorrect_answers": [
+              "Dave",
+              "Charles",
+              "Nathaniel"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "The stuffed tiger in Calvin and Hobbes is named after what philosopher?",
+            "correct_answer": "Thomas Hobbes",
+            "incorrect_answers": [
+              "David Hobbes",
+              "John Hobbes",
+              "Nathaniel Hobbes"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In Calvin and Hobbes, what is the name of Susie&#039;s stuffed rabbit?",
+            "correct_answer": "Mr. Bun",
+            "incorrect_answers": [
+              "Mr. Bunbun",
+              "Mr. Rabbit",
+              "Mr. Hoppy"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In Bionicle, who was formerly a Av-Matoran and is now the Toa of Light?",
+            "correct_answer": "Takua",
+            "incorrect_answers": [
+              "Jaller",
+              "Vakama",
+              "Tahu"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is Hellboy&#039;s true name?",
+            "correct_answer": "Anung Un Rama",
+            "incorrect_answers": [
+              "Right Hand of Doom",
+              "Ogdru Jahad",
+              "Azzael"
+            ]
+          },
+          {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What&#039;s the weakness of american vampires (Scott Snyder&#039;s American Vampire)?",
+            "correct_answer": "Gold",
+            "incorrect_answers": [
+              "Sunlight",
+              "Wood",
+              "Silver"
+            ]
+          }
+        ]
+      }
